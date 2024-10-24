1827b6e11f3e16ce7a3d488fc606b1214be136f1
Lucas Ribeiro <lucasfribeiro016@gmail.com>
2024-10-24 20:42:50 -0300

    24-10- Login e modal sucess

---
+++ src/js/home.js
@@ -0,0 +1,6 @@
+const closeSucess = document.getElementById('close_sucess');
+const modalSucess = document.getElementById('modal_sucess');
+
+closeSucess.addEventListener("click", () => {
+    modalSucess.style.display = "none"
+})
