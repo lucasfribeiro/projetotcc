a88eb765c47476e9aa10bf8c2d0634c74887cc57
Lucas Ribeiro <lucasfribeiro016@gmail.com>
2024-10-29 08:36:33 -0300

    29-10 - Sidebar e navbar

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,23 @@
+// Modal de sucesso
+
 const closeSucess = document.getElementById('close_sucess');
 const modalSucess = document.getElementById('modal_sucess');
 
 closeSucess.addEventListener("click", () => {
     modalSucess.style.display = "none"
+})
+
+// Abrir e fechar sidebar
+
+const openSidebar = document.getElementById('menu_btn');
+const closeSidebar = document.getElementById('close_sidebar');
+
+const sidebar = document.getElementById("sidebar")
+
+openSidebar.addEventListener("click", () => {
+    sidebar.style.display = "block"
+})
+
+closeSidebar.addEventListener("click", () => {
+    sidebar.style.display = "none"
 })
