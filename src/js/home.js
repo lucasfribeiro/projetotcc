8794bc09f11a1cc70d6272c9d2a16d6ec88efc4a
Lucas Ribeiro <lucasfribeiro016@gmail.com>
2024-10-29 11:44:33 -0300

    29-10 - Transição de telas

--- src/js/home.js
+++ src/js/home.js
@@ -21,3 +21,95 @@ openSidebar.addEventListener("click", () => {
 closeSidebar.addEventListener("click", () => {
     sidebar.style.display = "none"
 })
+
+// Transição de telas
+
+const Home = document.getElementById('home_page');
+const Receitas = document.getElementById('receitas_page');
+const Lembretes = document.getElementById('lembretes_page');
+const Perfil = document.getElementById('perfil_page');
+const Desempenho = document.getElementById('desempenho_page');
+const Options = document.getElementById('options_page');
+
+const homeBtn = document.getElementById('home'); 
+const receitasBtn = document.getElementById('receitas'); 
+const lembretesBtn = document.getElementById('lembretes'); 
+const perfilBtn = document.getElementById('perfil'); 
+const desempenhoBtn = document.getElementById('desempenho'); 
+const optionsBtn = document.getElementById('options'); 
+
+homeBtn.addEventListener("click", () => {
+
+    homeBtn.classList.add("active");
+
+    receitasBtn.classList.remove("active");
+    lembretesBtn.classList.remove("active");
+    perfilBtn.classList.remove("active");
+    desempenhoBtn.classList.remove("active");
+    optionsBtn.classList.remove("active");
+
+    Home.style.display = "block";
+
+    Receitas.style.display = "none";
+    Lembretes.style.display = "none";
+    Perfil.style.display = "none";
+    Desempenho.style.display = "none";
+    Options.style.display = "none";
+})
+
+receitasBtn.addEventListener("click", () => {
+
+    receitasBtn.classList.add("active");
+
+    homeBtn.classList.remove("active");
+    lembretesBtn.classList.remove("active");
+    perfilBtn.classList.remove("active");
+    desempenhoBtn.classList.remove("active");
+    optionsBtn.classList.remove("active");
+
+    Receitas.style.display = "block";
+
+    Home.style.display = "none";
+    Lembretes.style.display = "none";
+    Perfil.style.display = "none";
+    Desempenho.style.display = "none";
+    Options.style.display = "none";
+})
+
+lembretesBtn.addEventListener("click", () => {
+
+    lembretesBtn.classList.add("active");
+
+    homeBtn.classList.remove("active");
+    receitasBtn.classList.remove("active");
+    perfilBtn.classList.remove("active");
+    desempenhoBtn.classList.remove("active");
+    optionsBtn.classList.remove("active");
+
+    Lembretes.style.display = "block";
+
+    Home.style.display = "none";
+    Receitas.style.display = "none";
+    Perfil.style.display = "none";
+    Desempenho.style.display = "none";
+    Options.style.display = "none";
+})
+
+perfilBtn.addEventListener("click", () => {
+
+    perfilBtn.classList.add("active");
+
+    homeBtn.classList.remove("active");
+    receitasBtn.classList.remove("active");
+    lembrtesBtn.classList.remove("active");
+    desempenhoBtn.classList.remove("active");
+    optionsBtn.classList.remove("active");
+
+    Perfil.style.display = "block";
+
+    Home.style.display = "none";
+    Receitas.style.display = "none";
+    Lembretes.style.display = "none";
+    Desempenho.style.display = "none";
+    Options.style.display = "none";
+})
