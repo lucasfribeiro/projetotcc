5b122cbc706e0dedbef487b39845278ec0abf64b
Lucas Ribeiro <lucasfribeiro016@gmail.com>
2024-11-11 16:01:45 -0300

    breve update

--- src/js/home.js
+++ src/js/home.js
@@ -4,7 +4,7 @@ const closeSucess = document.getElementById('close_sucess');
 const modalSucess = document.getElementById('modal_sucess');
 
 closeSucess.addEventListener("click", () => {
-    modalSucess.style.display = "none"
+  modalSucess.style.display = "none"
 })
 
 // Abrir e fechar sidebar
